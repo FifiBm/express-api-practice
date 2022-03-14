9fe026c65605afb3fd2b18e0ca66bc4d4ad09a5c
FifiBm <fifibm90@gmail.com>
2022-03-14 16:58:23 -0700

    built a working express api with ajax to fetch data from the pokemon api and serve it to the html page

--- public/index.js
+++ public/index.js
@@ -1,2 +1,35 @@
 // Using jQuery and ajax make a get request to your express server
 // Display the retrieved data on the html page
+$(document).ready(function() {
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+
+        let userInput = ($('input[name="user-inp"]').val()).toLowerCase();
+
+        $('div').empty();
+
+        $.ajax({
+            method: 'POST',
+            url: '/form',
+            data: {
+                userInput
+            }
+        })
+        .done(function(msg) {
+            if (msg.serverResponse === undefined) {
+                const {
+                    name,
+                    sprites,
+                    id,
+                } = msg;
+                let pokeTemplate = `
+                    <h2>${id}. ${name}</h2>
+                    <img src="${sprites.front_default}">
+                `;
+                $('div').append(pokeTemplate);
+            } else {
+                alert(msg.serverResponse);
+            }
+         });
+    });
+});
--- server/index.js
+++ server/index.js
@@ -1,3 +1,31 @@
 // Create an express server that will serve up your static html file
 // Create a get request in your get route to the pokeapi requesting some data
 // Return that data to the front end and display it
+
+const express = require('express'); // require express dependency
+const app = express(); // invoke express function and assign to a variable
+const PORT = 3000; // The port the server will listen on
+const path = require('path');
+const axios = require('axios');
+
+app.use('/', express.static(path.join(__dirname, '..', 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post('/form', (req, res) => {
+    const { userInput } = req.body;
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${userInput}/`)
+    .then(response => {
+        res.status(200).send(response.data);
+    })
+    .catch(error => {
+        res.send({
+            serverResponse: 'Please enter a valid Pokemon name or ID!'
+        })
+    });
+
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}!`);
+});
