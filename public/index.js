867186fa522cfffc38e5ba1f74586ce5778f4990
M-A-Justice <michae1jus7ice@gmail.com>
2022-03-14 12:47:36 -0700

    init commit

---
+++ public/index.js
@@ -0,0 +1,2 @@
+// Using jQuery and ajax make a get request to your express server
+// Display the retrieved data on the html page
---
+++ server/index.js
@@ -0,0 +1,3 @@
+// Create an express server that will serve up your static html file
+// Create a get request in your get route to the pokeapi requesting some data
+// Return that data to the front end and display it
